54463921bed7bf598c0c225238950fc99aed1068
YuitoTakeuchi <tyuito0913@gmail.com>
2023-05-26 23:17:38 +0900

    umaku ikanai

--- main.cpp
+++ main.cpp
@@ -1,17 +1,38 @@
 #include "GaussQuadrature.hpp"
 #include <cmath>
 
-double f(double x) {
-    return x*x;
+double x(double t) {
+    return 1;
 }
 
-double f_o(double x) {
-    return f(x) / sqrt(1-x*x);
+Eigen::VectorXd x(Eigen::VectorXd t) {
+    Eigen::VectorXd ret = t;
+    for(int i = 0; i < ret.rows(); ++i) ret(i) = x(t(i));
+    return ret;
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    GaussQuadrature<LegendrePolynomial> gq;
-    std::cout << gq.integrate(n, -0.5, 0.5, &f_o) << std::endl;
+    const int n = 4;
+    // GaussQuadrature<LegendrePolynomial> gq;
+    // std::cout << gq.integrate(n, -0.5, 0.5, &f_o) << std::endl;
+    // Eigen::VectorXd weights, points;
+
+    GaussLegendreQuadrature<n> glq;
+    std::cout << glq.derivative << std::endl << std::endl;
+    std::cout << glq.points << std::endl << std::endl;
+
+    Eigen::VectorXd D = glq.derivative * x(glq.points);
+
+
+    // for(int i = 1; i <= n; ++i){
+    //     double t = glq.points(i);
+    //     double expected = 1;
+    //     double a = 0.0;
+    //     for(int j = 0; j <= n; ++j) {
+    //         a += glq.derivative(i-1, j) * x(glq.points(j));
+    //     }
+    //     std::cout << "x: " << glq.points(i) << " a: " << a << " expected: " << expected << "\n";
+    // }
+
+    std::cout << D << std::endl;
 }
