70ff59a87a1ae0190cb357263979aad50edde2bf
YuitoTakeuchi <tyuito0913@gmail.com>
2023-05-27 02:40:52 +0900

    feat add test

--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,11 @@
-#include "../include/GaussQuadrature.hpp"
+#include "GaussQuadrature.hpp"
 #include <gtest/gtest.h>
 
 namespace {
   class MyLibTest : public ::testing::Test{}; 
 
   TEST_F(MyLibTest, SqrFuncCheck) {
-    EXPECT_EQ(square(3.0), 9.0);
-    EXPECT_EQ(square(4.0), 16.0);
+    EXPECT_EQ((3.0) * (3.0), 9.0);
+    EXPECT_EQ((4.0) * (4.0), 16.0);
   }
-
-//   TEST_F(MyLibTest, SumFuncCheck) {
-//     EXPECT_EQ(sum(1.0, 2.0), 3.0);
-//   }
 }
